8114fc9b0a2b8e198d365d8d29d55c5bb429b450
Shakil Mahmud <almahmudruet17@gmail.com>
2021-10-04 21:53:15 +0600

    last commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const Header =()=> {
                <div className="col-12 d-flex justify-content-between"> 
                   <h1 className="ps-4 py-2">Learn English Today</h1>
                   <div className="d-flex">
-                      <Link className="text-white p-2 my-2 text-decoration-none hover:bg-white" to="/home">home</Link>
+                      <Link className="text-white p-2 my-2 text-decoration-none" to="/home">home</Link>
                       <Link className="text-white p-2 my-2 text-decoration-none" to="/About">About</Link>
                       <Link className="text-white p-2 my-2 text-decoration-none" to="/services">Service</Link>
                       <Link className="text-white p-2 my-2 text-decoration-none" to="/signup">Sign Up</Link>
